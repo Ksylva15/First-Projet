472966a7f02b2859c0fe1b33648b5dc5130bc97c
Ksylvason <ksylvason@gmail.com>
2024-08-05 15:56:44 +0200

    Mon premier vrai projet

---
+++ menu.php
@@ -0,0 +1,49 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Site produit</title>
+
+
+</head>
+<body>
+    <header>
+    	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+     
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                    </span>
+                </button>
+      <a href="index.php" class="navbar-brand" style="padding-left:10px ;">
+      <img src="img/03.jpg" alt="Bootstrap" width="50" height="50">
+    </a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto"> 
+                            <li class="nav-item">
+	                        <a href="listecontact.php" class="nav-link text-white"><strong>Mes contacts</strong></a>
+                            </li>
+                           <li class="nav-item">
+	                       <a href="ajoutercontact.php" class="nav-link text-white">Ajouter</a>
+                          </li>
+                       <li class="nav-item">
+	                   <a href="Rechercher.php" class="nav-link text-white">Rechercher</a>
+                           </li>
+                         <li class="nav-item">
+	                       <a href="index.php" class="nav-link text-white">Deconnexion</a>
+                         </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    
+</body>
+</html>
